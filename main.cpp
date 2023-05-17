0338723d14f214a35e5cb13939cc441ac9d954fa
fde-capu <flacarrara@gmail.com>
2023-05-16 23:48:25 -0300

    3 steps, ====|===------

--- main.cpp
+++ main.cpp
@@ -5,6 +5,9 @@
 #include <climits>
 
 #define MEMORY_TYPE_SIZE unsigned char
+#define THRESHOLD_DECAY 0.4
+#define OUTPUT_VALUE_DECAY 0.8
+#define ASCII_BAR_LENGTH 32
 
 template <typename T = MEMORY_TYPE_SIZE>
 T randomValue() {
@@ -25,7 +28,7 @@ private:
     double outputValueDecay; // New member variable for output value decay
 
 public:
-    Neuron(T* inputAddress, T* outputAddress, double thresholdDecay = 0.4, double outputValueDecay = 0.8)
+    Neuron(T* inputAddress, T* outputAddress, double thresholdDecay = THRESHOLD_DECAY, double outputValueDecay = OUTPUT_VALUE_DECAY)
         : inputAddress(inputAddress), outputAddress(outputAddress), thresholdDecay(thresholdDecay), outputValueDecay(outputValueDecay) {
         threshold = randomValue<T>();
         originalThreshold = threshold;
@@ -33,6 +36,10 @@ public:
 
     T getThreshold() const {
         return threshold;
+    }
+
+    T getOriginalThreshold() const {
+        return originalThreshold;
     }
 
     void writeRandomInputValue() {
@@ -51,6 +58,27 @@ public:
     }
 };
 
+template <typename T>
+void printAsciiBar(T inputValue, T threshold, T originalThreshold, int length) {
+    double scaleFactor = static_cast<double>(length) / std::numeric_limits<T>::max();
+    int scaledInputValue = static_cast<int>(inputValue * scaleFactor);
+    int scaledThreshold = static_cast<int>(threshold * scaleFactor);
+    int scaledOriginalThreshold = static_cast<int>(originalThreshold * scaleFactor);
+
+    for (int i = 0; i < length; i++) {
+        if (i == scaledThreshold) {
+            std::cout << '|';
+        } else if (i == scaledOriginalThreshold) {
+            std::cout << '*';
+        } else if (i < scaledInputValue) {
+            std::cout << '=';
+        } else {
+            std::cout << '-';
+        }
+    }
+    std::cout << std::endl;
+}
+
 int main() {
     srand(time(NULL));
 
@@ -60,17 +88,17 @@ int main() {
     Neuron<MEMORY_TYPE_SIZE> neuron(&inputMemory, &outputMemory);
 
     for (int i = 0; i < 3; i++) {
-        neuron.writeRandomInputValue(); // Write a random value to the input memory address
+        neuron.writeRandomInputValue();
 
         std::cout << "Run " << i + 1 << std::endl;
         std::cout << "Threshold: " << static_cast<int>(neuron.getThreshold()) << std::endl;
-        std::cout << "Initial input value: " << static_cast<int>(inputMemory) << std::endl;
-        std::cout << "Initial output value: " << static_cast<int>(outputMemory) << std::endl;
+        std::cout << "Input value: " << static_cast<int>(inputMemory) << std::endl;
+
+        printAsciiBar(inputMemory, neuron.getThreshold(), neuron.getOriginalThreshold(), ASCII_BAR_LENGTH);
 
         neuron.process();
 
-        std::cout << "After processing input value: " << static_cast<int>(inputMemory) << std::endl;
-        std::cout << "After processing output value: " << static_cast<int>(outputMemory) << std::endl;
+        std::cout << "Output value: " << static_cast<int>(outputMemory) << std::endl;
         std::cout << std::endl;
     }
 
