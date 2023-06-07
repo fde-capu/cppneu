b04602e3eea99bfaf383d69682028e3c11475468
fde-capu <flacarrara@gmail.com>
2023-06-07 15:53:18 -0300

    Menu progress.

--- menu.cpp
+++ menu.cpp
@@ -2,29 +2,31 @@
 #include "defines.hpp"
 #include "Being.hpp"
 
-typedef void (*voidFuncPtr)(void);
-
 void toggleShowMenu() { g_showMenu = !g_showMenu; }
 void toggleShowDebug() { g_showDebug = !g_showDebug; }
 void toggleRunning() { g_running = !g_running; }
+void gotoMenu(std::string);
+
+typedef void (*voidFuncPtr)(void);
 
 struct DescriptionFunction {
     std::string description;
     voidFuncPtr functionPtr;
 };
 
-std::map<char, DescriptionFunction> g_menu;
-
-void gotoMenu();
+static std::map<char, DescriptionFunction> g_menu;
+static std::string g_menu_name = "";
 
 static std::map<std::string, std::map<char, DescriptionFunction> >
 g_menu_tree = 
 {
-{ "main", {
+{ "global", {
 	{'d', {"debug", &toggleShowDebug}},
 	{'?', {"menu", &toggleShowMenu}},
 	{'q', {"quit", &doQuit}},
 	{'P', {"pause", &toggleRunning}},
+}},
+{ "main", {
 	{'h', {"header", &Being::toggleDisplayHeader}},
 	{'c', {"chars", &Being::toggleDisplayCharacters}},
 	{'p', {"physical", &Being::toggleDisplayPhysical}},
@@ -32,28 +34,33 @@ g_menu_tree =
 	{'a', {"action", &Being::toggleDisplayAction}},
 	{'m', {"measures", &Being::toggleDisplayMeasure}},
 	{'w', {"want", &Being::toggleDisplayActionResolution}},
-	{'b', {"bars up", &Being::toggleDisplayBarsUp}},
-	{'B', {"bars menu", &gotoMenu}},
+	{'D', {"display", []() -> void { gotoMenu("display"); }}},
+}},
+{ "display", {
+	{'D', {"back", &menuInit}},
+	{'n', {"numbers", []() -> void { Being::toggleBit(DISPLAY_NUMBERS); }}},
+	{'b', {"bars", []() -> void { Being::toggleBit(DISPLAY_BAR); }}},
+	{'c', {"characters", []() -> void { Being::toggleBit(DISPLAY_CHARACTER); }}},
+	{'d', {"description", []() -> void { Being::toggleBit(DISPLAY_DESCRIPTION); }}},
 	{'o', {"outs", &Being::toggleDisplayOuts}},
 	{'x', {"axons", &Being::toggleDisplayAxons}},
 	{'i', {"bias bars", &Being::toggleDisplayBiasBars}},
-//	{' ', {.description = "new", &makeBrain}},
-}},
-{ "bars", {
-	{'B', {"back", &menuInit}},
-  {'n', {"numbers", &toggleShowMenu}},
-  {'b', {"bars", &toggleShowMenu}},
+	{'+', {"up", &Being::toggleDisplayBarsUp}},
+	{'-', {"down", &Being::toggleDisplayBarsDown}},
 }}
 };
 
-void gotoMenu()
+static std::map<char, DescriptionFunction> g_menu_global = g_menu_tree["global"];
+
+void gotoMenu(std::string u_menu)
 {
-	g_menu = g_menu_tree["bars"];
+	g_menu = g_menu_tree[u_menu];
+	g_menu_name = u_menu;
 }
 
 void menuInit()
 {
-	g_menu = g_menu_tree["main"];
+	gotoMenu("main");
 }
 
 std::string g_debugString("");
@@ -69,15 +76,18 @@ void printDebug()
 void printMenu()
 {
 	if (!g_showMenu) return;
-	printw(":");
-	for (const auto& kv : g_menu) {
+	printw("%s ", g_menu_name.c_str());
+	for (const auto& kv : g_menu_global)
 		printw("%c:%s ", kv.first, kv.second.description.c_str());
-	}
-	printw(":\n");
+	for (const auto& kv : g_menu)
+		printw("%c:%s ", kv.first, kv.second.description.c_str());
+	printw("\n");
 }
 
 void dealKeyPress(int ch)
 {
+	if (g_menu_global.find(ch) != g_menu_global.end())
+		g_menu_global[ch].functionPtr();
 	if (g_menu.find(ch) != g_menu.end())
 		g_menu[ch].functionPtr();
 }
